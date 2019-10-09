a4a5a6e2d36a20623259045bf322e95cc2b7d4df
UgAr-study <55754822+UgAr-study@users.noreply.github.com>
2019-10-09 10:59:45 +0300

    h

--- main.c
+++ main.c
@@ -1,76 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <srting.h>
 #include <assert.h>
-#include <locale.h>
 
-int Compare(int j, int j + 1, int* index, char* text){                                          //if j-string > (j + 1)-string return 1, else return -1;
-    int String_size = min((index[j+1] - index[j]), (index[j+2] - index[j + 1]);
-    for (int i = 0; i < String_size; i++){
-        if (*(index[j] + i) > *(index[j + 1] + i))  return 1;
-        if (*(index[j] + i) < *(index[j + 1] + i))  return -1;
-    }
-    if ((index[j+1] - index[j]) > (index[j+2] - index[j + 1])) return 1;
-    else return -1;
-}
+#define TRUE 1
+#define FALSE 0
 
-int Reading(){
-    FILE * Onegin = fopen("Onegin.txt", "r");
+char* Read(char** index, char* text){
+
+    FILE * Onegin = fopen ("Onegin.txt", "rb");
+    //FILE * Onegin_Sort = fopen ("Onegin_Sort.txt", "w");
+
+    long long size_of_file = 0;
+
     if (Onegin == NULL) printf("Error of file\n");
-    else {
+
+    else{
         fseek (Onegin, 0, SEEK_END);
-        long size_of_file = ftell (Onegin);
+        size_of_file = ftell (Onegin);
+        fseek (Onegin, 0, SEEK_SET);
     }
 
-    char* text = (char*) calloc (size_of_file + 1, sizeof(char));
-    size_t length = fread (text, sizeof(char), Onegin);
+    text = (char*) realloc (text, (size_of_file + 1) * sizeof(char));
+    assert (text != NULL);
 
-    assert (length == size_of_file);
+    size_t length = fread (text, sizeof(char), size_of_file, Onegin);
+    assert (size_of_file == length);
 
-    int string_number = 0;
-    long long* index = (long long*) calloc (size_of_file, sizeof(long long));
-    index[0] = text;
+    printf("size of file = %lld\n", size_of_file);
 
-    for (int i = 1; i < size_of_file; i++){
-        for (int j = 0; j < size_of_file; j++)
-            if (text[j] == '\n'){                                    //don't forget about /r!!!
-                index[i] = (text + j + 1);
-                string_number++;
-                i++;                                                //write adress
-            }
-        break;
-    }
+    index = (char**) realloc (index, size_of_file * sizeof(char*));
+    assert (index != NULL);
 
-    index = (int*) realloc (index, (string_number + 1) * sizeof(long long));
+    *index = text;
 
-    for (int i = 0; i < string_number; i++){
-        for (int j = i; j < string_number; j++){
-            if (Compare (j, j + 1, index, text) == 1){              //if j-string > (j + 1)-string swap their adresses
-                int t = index[j];
-                index[j] = index[j + 1];
-                index[j + 1] = t;
-            }
+    printf("text[seek] = %p\n", index[0]);
+
+//    printf ("\n%d", length);
+//    printf ("%s\n", text);
+
+    //fwrite(text, sizeof(char), size_of_file, Onegin_Sort);
+
+
+    int number_of_strings = 1;
+
+    for(long i = 0; i < size_of_file; i++){
+        if(*(text + i) == '\n'){
+            *(text + i) = '\0';
+            index[number_of_strings] = (text + i + 1);
+
+            int c = *(index[number_of_strings]);
+            printf("text[seek] = %p\t char is %c\n", index[number_of_strings], c);
+
+            number_of_strings++;
         }
     }
-    return length;
+
+    index = (char**) realloc (index, number_of_strings * sizeof(char*));
+
+    printf("number of strings = %d\n", number_of_strings);
+
+    printf("text[seek] after realloc = %p\n", index[0]);
+
+    fclose(Onegin);
+
+    return text;
 }
 
-void Output (char* text, int* index, ){
-    char* Sort_text = (char*) calloc (size_of_file + 1, sizeof(char));
-    for (int i = 0; i < ; i++){
-        int String_lengh =
-        for (j = 0; j < ; j++){
+int Is_Letter(char c){
+    if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) return TRUE;
+    else {
+        printf("%c is not a char\n", c);
+        return FALSE;
+    }
+}
 
+int Compare (int j, char** index){
+
+    !!!!!printf("the beginning of the string is %p\n", index[0]);
+
+    int i = 0;      //i for j-string
+    for (int r = 0; ; r++){      //r for (j + 1) - string
+
+        int I = -777, R = -777;
+        char c = *(index[j] + i);
+        if ((I = Is_Letter (c)) == FALSE){
+            while ((I = Is_Letter (c)) == FALSE){
+
+                if ( c == '\r' || c == '\0') return FALSE;
+                i++;
+                c = *(index[j] + i);
+            }
         }
+
+        if ((R = Is_Letter (*(*(index + j + 1) + r * sizeof(char)))) == FALSE){
+           while ((R = Is_Letter (* (*(index + j + 1) + r * sizeof(char)))) == FALSE){
+                printf("R\n");
+                if ( *(*(index + j + 1) + r * sizeof(char)) == '\r' || *(*(index + j + 1) + r * sizeof(char)) == '\0') return TRUE;
+                r++;
+            }
+        }
+
+        if ( *(*(index + j) + i * sizeof(char)) > *(*(index + j + 1) + r * sizeof(char))) return TRUE;
+        if ( *(*(index + j) + i * sizeof(char)) < *(*(index + j + 1) + r * sizeof(char))) return FALSE;
+        i++;
+
     }
 }
 
 int main()
 {
-    printf("%d", Reading());                                        //check reading
+
+    //FILE * Onegin_Sort = fopen ("Onegin_Sort.txt", "w");
+
+    char* text = (char*) calloc (1, sizeof(char));
+    assert (text != NULL);
+
+    char** index = (char**) calloc (1, sizeof(char*));
+    assert (index != NULL);
+
+   !!!!!! printf("check theee first add = %p\n", index[0]);
+
+    Read(index, text);
+
+    int y = Compare(0, index);
+    printf("Let's compare 1 and 2 strings %d\n", y);
+    //Write();
 
 
-
+    //fclose(Onegin_Sort);
+    //free(text);
     return 0;
 }
+
 
