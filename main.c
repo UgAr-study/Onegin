cc9d2dd92b98e7ce6ad694a45df0fce69f8751fb
UgAr-study <55754822+UgAr-study@users.noreply.github.com>
2019-10-25 02:57:28 +0300

    It works!!! Sorting text in alphabet
    it didn't work cause of after comparing left & middle CONTINUE wasn't written( so next 'if' was true(

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 char* Read(char*** index, char** text, int* index_size){
 
     FILE * Onegin = fopen ("Onegin.txt", "rb");
-    //FILE * Onegin_Sort = fopen ("Onegin_Sort.txt", "w");
 
     long long size_of_file = 0;
 
@@ -16,56 +15,42 @@ char* Read(char*** index, char** text, int* index_size){
 
     else{
         fseek (Onegin, 0, SEEK_END);
+        //size_of_file = filelenght (fileno (Onegin));
         size_of_file = ftell (Onegin);
         fseek (Onegin, 0, SEEK_SET);
     }
 
-    *text = (char*) realloc (*text, (size_of_file + 1) * sizeof(char));
+    *text = (char*) calloc ((++size_of_file), sizeof(char));
     assert (text != NULL);
 
+
     size_t length = fread (*text, sizeof(char), size_of_file, Onegin);
-    assert (size_of_file == length);
+    assert (size_of_file == length + 1);
+
+    for (int i = 0; i < size_of_file; i++)
+        printf ("text[%d] = %d\n", i, (*text)[i]);
 
     printf("size of file = %lld\n", size_of_file);
 
     *index = (char**) calloc (size_of_file, sizeof(char*));
     assert (index != NULL);
 
-    *index[0] = *text;
+    (*index)[0] = *text;
 
-    printf ("%s\n", *text);
-    //printf("text[seek] = %p\t char is %c\n", *index[0], **index[0]);
-
-    //fwrite(text, sizeof(char), size_of_file, Onegin_Sort);
-
+    printf ("text is:\n%s\n", *text);
 
     int number_of_strings = 1;
 
     for(long i = 0; i < size_of_file; i++){
 
-        //printf("I came to 'for' in Read! %c\n", (*text)[i]);
-        int flag = 0;
         if((*text)[i] == '\r'){
-            //printf("I came to 'if' in 'for' in Read!\n");
-            flag = 1;
+
             (*text)[i] = '\0';
             (*index)[number_of_strings] = *text + i + 2;
-
-            //char c = *((*index)[number_of_strings]);
-            //printf("text[seek] = %p\t char is %c\n", (*index)[number_of_strings], c);
 
             number_of_strings ++;
         }
 
-        if((*text)[i] == '\n' && flag == 0){
-            (*text)[i] = '\0';
-
-            if(i < size_of_file - 1)
-                (*index)[number_of_strings] = *text + i + 1;
-        }
-        flag = 0;
-
-        //printf("I came out to 'for' in Read!\n\n");
     }
 
     *index_size = number_of_strings;
@@ -73,8 +58,6 @@ char* Read(char*** index, char** text, int* index_size){
     *index = (char**) realloc (*index, number_of_strings * sizeof(char*));
 
     printf("number of strings = %d\n", number_of_strings);
-
-    //printf("index[1] after realloc = %p\n", (*index)[1]);
 
     fclose(Onegin);
 
@@ -166,14 +149,10 @@ void Qsort(char*** index, int a_begin, int a_end){
     puts((*index)[middle]);
     printf("\n");
 
-    while (right > left) {
+    while (right >= left) {
         //printf("In high while\n");
         //printf("left = %d\n", left);
         while (Compare(middle, left, *index)){
-
-            //printf("middle = ");
-            //puts((*index)[middle]);
-            //printf("\n");
 
             printf("In low while for left\t index[%d] = %c\n\n", left, *(*index)[left]);
             left++;
@@ -181,32 +160,37 @@ void Qsort(char*** index, int a_begin, int a_end){
 
         while (Compare(right, middle, *index)){
 
-            //printf("middle = ");
-            //puts((*index)[middle]);
-            //printf("\n");
-
             printf("In low while for right\t index[%d] = %c\n\n", right, *(*index)[right]);
             right--;
         }
 
-        if(right > left){
+        if(right >= left){
 
             if(left == middle){
                 char* t = (*index)[right];
                 (*index)[right] = (*index)[left];
                 (*index)[left] = t;
+
                 middle = right;
+                printf("\nmiddle = ");
+                puts((*index)[middle]);
+                printf("\n");
 
                 printf("after swap in l = m: index[%d] = %c index[%d] = %c\n", left, *(*index)[left], right, *(*index)[right]);
                 left++;
                 right--;
+                continue;
             }
 
             if(right == middle){
                 char* t = (*index)[right];
                 (*index)[right] = (*index)[left];
                 (*index)[left] = t;
+
                 middle = left;
+                printf("middle = ");
+                puts((*index)[middle]);
+                printf("\n");
 
                 printf("after swap in r = m: index[%d] = %c index[%d] = %c\n", left, *(*index)[left], right, *(*index)[right]);
                 left++;
@@ -225,9 +209,10 @@ void Qsort(char*** index, int a_begin, int a_end){
 
     //printf("left = %d\t right = %d\n", left, right);
     if(left - right == 2){
+        printf("left - right = 2");
         if(Compare(right + 1, middle, *index))
             left--;
-        else right ++;
+        else right++;
     }
 
     Qsort (index, a_begin, right);
